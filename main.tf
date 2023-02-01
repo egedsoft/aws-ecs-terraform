34b1163134235d87746cecb4de21f00b381972c9
Guy Mac <guy@egedsoft.co.il>
2023-02-01 11:29:40 +0200

    update

--- main.tf
+++ main.tf
@@ -8,36 +8,36 @@ data "aws_region" "current" {}
 module "sg_alb" {
   source = "./modules/sg"
 
-  sg_name        = "alb-sg"
+  sg_name        = var.alb_sg_name
   sg_description = "controls access to the ALB"
 
   vpc_id = data.aws_vpc.selected.id
 
   ingress_cidr_blocks_list = var.ingress_cidr_blocks_list
   ingress_app_port         = var.ingress_app_port
-  ingress_protocol         = "tcp"
+  ingress_protocol         = var.ingress_protocol
 
-  egress_cidr_blocks_list = ["0.0.0.0/0"]
-  egress_app_port         = 0
-  egress_protocol         = "-1"
+  egress_cidr_blocks_list = var.egress_cidr_blocks_list
+  egress_app_port         = var.egress_app_port
+  egress_protocol         = var.egress_protocol
 
 }
 
 module "sg_ecs" {
   source = "./modules/sg"
 
-  sg_name        = "ecs-sg"
+  sg_name        = var.ecs_sg_name
   sg_description = "controls access to the ECS"
 
   vpc_id = data.aws_vpc.selected.id
 
-  ingress_app_port        = 80
-  ingress_protocol        = "tcp"
+  ingress_app_port        = var.ingress_app_port
+  ingress_protocol        = var.ingress_protocol
   ingress_security_groups = [module.sg_alb.sg_id]
 
-  egress_cidr_blocks_list = ["0.0.0.0/0"]
-  egress_app_port         = 0
-  egress_protocol         = "-1"
+  egress_cidr_blocks_list = var.egress_cidr_blocks_list
+  egress_app_port         = var.egress_app_port
+  egress_protocol         = var.egress_protocol
 
 }
 
@@ -59,11 +59,11 @@ module "iam" {
 
 module "ecs" {
   source               = "./modules/ecs"
-  cluster_name         = "ecs1"
-  service_name         = "testapp-service"
+  cluster_name         = var.ecs_cluster_name
+  service_name         = var.ecs_service_name
   app_count            = var.app_count
-  app_image            = "nginx:latest"
-  app_port             = 80
+  app_image            = var.app_image
+  app_port             = var.ingress_app_port
   execution_role_arn   = module.iam.ecs_task_execution_role_arn
   fargate_memory       = var.fargate_memory
   fargate_cpu          = var.fargate_cpu
--- variables.tf
+++ variables.tf
@@ -48,3 +48,48 @@ variable "ecs_task_execution_role" {
   default     = ""
   description = "ECS task execution role name"
 }
+
+
+
+variable "ingress_app_port" {
+  description = "Port of the application"
+  type        = number
+}
+
+variable "ingress_cidr_blocks_list" {
+  description = "List of CIDR blocks for ingress"
+  type        = list(string)
+  default     = []
+}
+
+variable "ingress_protocol" {
+  description = "Ingress Protocol (tcp/udp/-1)"
+  type        = string
+  default     = ""
+}
+
+
+variable "egress_app_port" {
+  description = "Port of the application"
+  type        = number
+  default     = 0
+}
+
+variable "egress_cidr_blocks_list" {
+  description = "List of CIDR blocks for egress"
+  type        = list(string)
+  default     = []
+}
+
+variable "egress_protocol" {
+  description = "Egress Protocol (tcp/udp/-1)"
+  type        = string
+  default     = ""
+}
+
+
+variable "ecs_sg_name" {}
+variable "alb_sg_name" {}
+variable "app_image" {}
+variable "ecs_service_name" {}
+variable "ecs_cluster_name" {}
